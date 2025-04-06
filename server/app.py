73c67a5bc0c46ba6971bd3805941aab4c339f6c8
Steamgx <your-bmuraya646@gmail.com>
2025-04-06 23:28:59 +0300

    commits

--- server/app.py
+++ server/app.py
@@ -2,8 +2,6 @@
 
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -16,42 +14,52 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-api = Api(app)
+@app.route('/clear', methods=['DELETE'])
+def clear_session():
+    session['page_views'] = None
+    session['user_id'] = None
+    return {}, 204
 
-class ClearSession(Resource):
+@app.route('/articles', methods=['GET'])
+def index_article():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return articles, 200
 
-    def delete(self):
+@app.route('/articles/<int:id>', methods=['GET'])
+def show_article(id):
+    session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.filter(Article.id == id).first()
+        article_json = jsonify(article.to_dict())
+        return make_response(article_json, 200)
+
+    return {'message': 'Maximum pageview limit reached'}, 401
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return {'message': 'User not found'}, 404
     
-        session['page_views'] = None
-        session['user_id'] = None
+    session['user_id'] = user.id
+    return user.to_dict(), 200
 
-        return {}, 204
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    session['user_id'] = None
+    return {}, 204
 
-class IndexArticle(Resource):
+@app.route('/check_session', methods=['GET'])
+def check_session():
+    user_id = session.get('user_id')
+    if not user_id:
+        return {}, 401
     
-    def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
-
-class ShowArticle(Resource):
-
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
+    user = db.session.get(User, user_id)
+    return user.to_dict(), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
